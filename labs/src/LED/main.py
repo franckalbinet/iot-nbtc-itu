e4494b88c4d867f1d23bce0ee2cd8336986ad91a
Franck Albinet <franckalbinet@gmail.com>
2017-11-09 15:44:55 +0100

    zip python resources

--- labs/src/LED/main.py
+++ labs/src/LED/main.py
@@ -3,8 +3,8 @@ import time
 pycom.heartbeat(False)
 for cycles in range(10): # stop after 10 cycles
     pycom.rgbled(0x007f00) # green
-    time.sleep(5)
+    time.sleep(1)
     pycom.rgbled(0x7f7f00) # yellow
-    time.sleep(5)
+    time.sleep(1)
     pycom.rgbled(0x7f0000) # red
-    time.sleep(5)
+    time.sleep(1)
